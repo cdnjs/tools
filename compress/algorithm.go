7bcdfb683ef241096f729e7503bfbfa2c5ec3efb
tylercaslin <tcaslin@cloudflare.com>
2020-07-07 12:19:18 -0400

    remove brotli native, remove gzip cli, go mod tidy

--- compress/algorithm.go
+++ compress/algorithm.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os/exec"
 
-	"github.com/andybalholm/brotli"
 	"github.com/cdnjs/tools/util"
 )
 
@@ -33,26 +32,6 @@ func runAlgorithm(alg string, args ...string) []byte {
 // at optimal compression (quality 11).
 func Brotli11CLI(filePath string) []byte {
 	return runAlgorithm("brotli", "-c", "-q", "11", filePath)
-}
-
-// Gzip9CLI returns a gzip compressed file as bytes
-// at optimal compression (level 9).
-func Gzip9CLI(filePath string) []byte {
-	return runAlgorithm("gzip", "-c", "-9", filePath)
-}
-
-// Brotli11Native returns a brotli compressed file as bytes
-// at optimal compression (quality 11).
-func Brotli11Native(uncompressed []byte) []byte {
-	var b bytes.Buffer
-
-	w := brotli.NewWriterLevel(&b, brotli.BestCompression)
-
-	_, err := w.Write(uncompressed)
-	util.Check(err)
-	util.Check(w.Close())
-
-	return b.Bytes()
 }
 
 // Gzip9Native returns a gzip compressed file as bytes
