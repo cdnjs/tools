e23b4c04c75e1d46ff417c83489ac57bcaa6be93
tylercaslin <tcaslin@cloudflare.com>
2020-07-21 12:45:28 -0400

    basic metadata structs

--- kv/metadata.go
+++ kv/metadata.go
@@ -3,6 +3,10 @@ package kv
 import (
 	"context"
 	"encoding/json"
+	"path"
+	"sort"
+
+	"github.com/cdnjs/tools/util"
 )
 
 const (
@@ -62,10 +66,85 @@ func GetVersion(key string) (Version, error) {
 	return v, err
 }
 
+// Perform a binary search, inserting a string into the sorted list if not present.
+func insertToSortedListIfNotPresent(sorted []string, s string) []string {
+	i := sort.SearchStrings(sorted, s)
+	if i == len(sorted) {
+		return append(sorted, s) // insert at back of list
+	}
+	if sorted[i] == s {
+		return sorted // already exists in list
+	}
+	return append(sorted[:i], append([]string{s}, sorted[i:]...)...) // insert into list
+}
+
+// Gets the request to update the root metadata entry in KV with a new package.
+func updateRootRequest(pkg string) *writeRequest {
+	r, err := GetRoot()
+	if err != nil {
+		// assume key not found or malformed JSON
+		// so we will rewrite this entry
+		// FIX THIS -- NO ASSUMPTIONS -> PARSE THE ERROR!
+		r.Packages = []string{pkg}
+	} else {
+		r.Packages = insertToSortedListIfNotPresent(r.Packages, pkg)
+	}
+
+	v, err := json.Marshal(r)
+	util.Check(err)
+
+	return &writeRequest{
+		key:   rootKey,
+		value: v,
+	}
+}
+
+// Gets the request to update a package metadata entry in KV with a new version.
+func updatePackageRequest(pkg, version string) *writeRequest {
+	key := pkg
+	p, err := GetPackage(key)
+	if err != nil {
+		// assume key not found or malformed JSON
+		// so we will rewrite this entry
+		// FIX THIS -- NO ASSUMPTIONS -> PARSE THE ERROR!
+		p.Versions = []string{version}
+	} else {
+		p.Versions = insertToSortedListIfNotPresent(p.Versions, version)
+	}
+
+	// TODO: Add additional metadata found in package.min.js generation.
+
+	v, err := json.Marshal(p)
+	util.Check(err)
+
+	return &writeRequest{
+		key:   key,
+		value: v,
+	}
+}
+
+// Gets the request to update a version entry in KV with a number of file assets.
+func updateVersionRequest(pkg, version string, files []string) *writeRequest {
+	key := path.Join(pkg, version)
+
+	sort.Strings(files)
+	v, err := json.Marshal(files)
+	util.Check(err)
+
+	return &writeRequest{
+		key:   key,
+		value: v,
+	}
+}
+
 // Updates KV with new version's metadata, writing to all of the necessary data structures.
 // The []string of `fromVersionPaths` will already contain the optimized/minified files by now.
 func updateKVMetadata(ctx context.Context, pkg, version string, fromVersionPaths []string) error {
 	var kvs []*writeRequest
+
+	kvs = append(kvs, updateRootRequest(pkg))
+	kvs = append(kvs, updatePackageRequest(pkg, version))
+	kvs = append(kvs, updateVersionRequest(pkg, version, fromVersionPaths))
 
 	// write bulk to KV
 	return encodeAndWriteKVBulk(ctx, kvs, metadataNamespaceID)
