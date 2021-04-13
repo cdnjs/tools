594a7fa57496e49ded123584076dd8c4f9cb05f4
Sven Sauleau <sven@cloudflare.com>
2021-04-13 11:15:39 +0100

    compress in worker pool

---
+++ compress/worker.go
@@ -0,0 +1,29 @@
+package compress
+
+import (
+	"context"
+	"path"
+)
+
+type CompressJob struct {
+	Ctx         context.Context
+	File        string
+	VersionPath string
+}
+
+func Worker(jobs <-chan CompressJob) {
+	for j := range jobs {
+		switch path.Ext(j.File) {
+		case ".jpg", ".jpeg":
+			Jpeg(j.Ctx, path.Join(j.VersionPath, j.File))
+		case ".png":
+			Png(j.Ctx, path.Join(j.VersionPath, j.File))
+		case ".js":
+			Js(j.Ctx, path.Join(j.VersionPath, j.File))
+		case ".css":
+			CSS(j.Ctx, path.Join(j.VersionPath, j.File))
+		default:
+			panic("unreachable: " + j.File)
+		}
+	}
+}
