c287d171e0fcd676d4dbbfcaca58d65fea26043e
Sven Sauleau <sven@cloudflare.com>
2021-04-28 23:26:48 +0100

    add cloud functions and process-version

---
+++ functions/kv-pump/main.go
@@ -0,0 +1,200 @@
+package kv_pump
+
+import (
+	"archive/tar"
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"cloud.google.com/go/functions/metadata"
+	"cloud.google.com/go/storage"
+	cloudflare "github.com/cloudflare/cloudflare-go"
+	"github.com/pkg/errors"
+)
+
+var (
+	KV_TOKEN      = os.Getenv("KV_TOKEN")
+	CF_ACCOUNT_ID = os.Getenv("CF_ACCOUNT_ID")
+	KV_NAMESPACE  = os.Getenv("KV_NAMESPACE")
+)
+
+// GCSEvent is the payload of a GCS event.
+type GCSEvent struct {
+	Kind                    string                 `json:"kind"`
+	ID                      string                 `json:"id"`
+	SelfLink                string                 `json:"selfLink"`
+	Name                    string                 `json:"name"`
+	Bucket                  string                 `json:"bucket"`
+	Generation              string                 `json:"generation"`
+	Metageneration          string                 `json:"metageneration"`
+	ContentType             string                 `json:"contentType"`
+	TimeCreated             time.Time              `json:"timeCreated"`
+	Updated                 time.Time              `json:"updated"`
+	TemporaryHold           bool                   `json:"temporaryHold"`
+	EventBasedHold          bool                   `json:"eventBasedHold"`
+	RetentionExpirationTime time.Time              `json:"retentionExpirationTime"`
+	StorageClass            string                 `json:"storageClass"`
+	TimeStorageClassUpdated time.Time              `json:"timeStorageClassUpdated"`
+	Size                    string                 `json:"size"`
+	MD5Hash                 string                 `json:"md5Hash"`
+	MediaLink               string                 `json:"mediaLink"`
+	ContentEncoding         string                 `json:"contentEncoding"`
+	ContentDisposition      string                 `json:"contentDisposition"`
+	CacheControl            string                 `json:"cacheControl"`
+	Metadata                map[string]interface{} `json:"metadata"`
+	CRC32C                  string                 `json:"crc32c"`
+	ComponentCount          int                    `json:"componentCount"`
+	Etag                    string                 `json:"etag"`
+	CustomerEncryption      struct {
+		EncryptionAlgorithm string `json:"encryptionAlgorithm"`
+		KeySha256           string `json:"keySha256"`
+	}
+	KMSKeyName    string `json:"kmsKeyName"`
+	ResourceState string `json:"resourceState"`
+}
+
+func Invoke(ctx context.Context, e GCSEvent) error {
+	meta, err := metadata.FromContext(ctx)
+	if err != nil {
+		return fmt.Errorf("metadata.FromContext: %v", err)
+	}
+	log.Printf("Event ID: %v\n", meta.EventID)
+	log.Printf("Event type: %v\n", meta.EventType)
+	log.Printf("File: %v\n", e.Name)
+	log.Printf("Metadata: %v\n", e.Metadata)
+
+	// pkg := e.Metadata["package"].(string)
+	// version := e.Metadata["version"].(string)
+	pkg := "hi-sven"
+	version := "1.10.0"
+
+	archive, err := readObject(ctx, e.Bucket, e.Name)
+	if err != nil {
+		return fmt.Errorf("could not read object: %v", err)
+	}
+
+	cfapi, err := cloudflare.NewWithAPIToken(KV_TOKEN, cloudflare.UsingAccount(CF_ACCOUNT_ID))
+	if err != nil {
+		return errors.Wrap(err, "failed to create cloudflare API client")
+	}
+
+	var pairs []*writeRequest
+
+	onFile := func(name string, r io.Reader) error {
+		// remove leading slash
+		key := fmt.Sprintf("%s/%s/%s", pkg, version, name[1:])
+		bytes, err := ioutil.ReadAll(r)
+		if err != nil {
+			return errors.Wrap(err, "could not read file")
+		}
+
+		meta := newMetadata(len(bytes))
+		writePair := &writeRequest{
+			key:   key,
+			name:  key,
+			value: bytes,
+			meta:  meta,
+		}
+		metaStr, _ := json.Marshal(meta)
+		log.Printf("%s (%s)\n", key, metaStr)
+		pairs = append(pairs, writePair)
+		return nil
+	}
+	if err := inflate(bytes.NewReader(archive), onFile); err != nil {
+		return fmt.Errorf("could not inflate archive: %s", err)
+	}
+
+	res, err := encodeAndWriteKVBulk(ctx, cfapi, pairs, KV_NAMESPACE, false)
+	if err != nil {
+		return fmt.Errorf("failed to write KV: %s", err)
+	}
+	log.Println(res)
+
+	return nil
+}
+
+func encodeToBase64(bytes []byte) string {
+	return base64.StdEncoding.EncodeToString(bytes)
+}
+func newMetadata(size int) *FileMetadata {
+	lastModifiedTime := time.Now()
+	lastModifiedSeconds := lastModifiedTime.UnixNano() / int64(time.Second)
+	lastModifiedStr := lastModifiedTime.Format(http.TimeFormat)
+	etag := fmt.Sprintf("%x-%x", lastModifiedSeconds, size)
+
+	return &FileMetadata{
+		ETag:         etag,
+		LastModified: lastModifiedStr,
+	}
+}
+
+func readObject(ctx context.Context, bucket string, name string) ([]byte, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create client")
+	}
+
+	bkt := client.Bucket(bucket)
+	obj := bkt.Object(name)
+
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create client")
+	}
+	defer r.Close()
+
+	var buff bytes.Buffer
+	w := bufio.NewWriter(&buff)
+
+	if _, err := io.Copy(w, r); err != nil {
+		return nil, errors.Wrap(err, "could not read object")
+	}
+
+	return buff.Bytes(), nil
+}
+
+func inflate(gzipStream io.Reader, onFile func(string, io.Reader) error) error {
+	uncompressedStream, err := gzip.NewReader(gzipStream)
+	if err != nil {
+		log.Fatal("ExtractTarGz: NewReader failed")
+	}
+
+	tarReader := tar.NewReader(uncompressedStream)
+
+	for {
+		header, err := tarReader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("ExtractTarGz: Next() failed: %s", err.Error())
+		}
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			// do nothing
+		case tar.TypeReg:
+			if err := onFile(header.Name, tarReader); err != nil {
+				return errors.Wrap(err, "failed to handle file")
+			}
+		default:
+			return errors.Errorf(
+				"ExtractTarGz: uknown type: %x in %s",
+				header.Typeflag,
+				header.Name)
+		}
+	}
+	return nil
+}
--- kv/aggregate.go
+++ kv/aggregate.go
@@ -1,75 +1,75 @@
 package kv
 
-import (
-	"context"
-	"encoding/json"
-	"fmt"
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"fmt"
 
-	"github.com/cdnjs/tools/compress"
-	"github.com/cdnjs/tools/util"
+// 	"github.com/cdnjs/tools/compress"
+// 	"github.com/cdnjs/tools/util"
 
-	"github.com/cdnjs/tools/packages"
-)
+// 	"github.com/cdnjs/tools/packages"
+// )
 
 // UpdateAggregatedMetadata updates a package's KV entry for aggregated metadata.
 // Returns the keys written to KV, whether the existing entry was found, and if there were any errors.
-func UpdateAggregatedMetadata(ctx context.Context, pckg *packages.Package, newAssets []packages.Asset) ([]string, bool, error) {
-	aggPckg, err := getAggregatedMetadata(*pckg.Name)
-	var found bool
-	if err != nil {
-		switch err.(type) {
-		case KeyNotFoundError:
-			{
-				// key not found (new package)
-				util.Debugf(ctx, "KV key `%s` not found, inserting aggregated metadata...\n", *pckg.Name)
-				pckg.Assets = newAssets
-			}
-		default:
-			{
-				return nil, false, err
-			}
-		}
-	} else {
-		util.Debugf(ctx, "Aggregated metadata for `%s` found. Updating aggregated metadata...\n", *pckg.Name)
-		pckg.Assets = append(aggPckg.Assets, newAssets...)
-		found = true
-	}
+// func UpdateAggregatedMetadata(ctx context.Context, pckg *packages.Package, newAssets []packages.Asset) ([]string, bool, error) {
+// 	aggPckg, err := getAggregatedMetadata(*pckg.Name)
+// 	var found bool
+// 	if err != nil {
+// 		switch err.(type) {
+// 		case KeyNotFoundError:
+// 			{
+// 				// key not found (new package)
+// 				util.Debugf(ctx, "KV key `%s` not found, inserting aggregated metadata...\n", *pckg.Name)
+// 				pckg.Assets = newAssets
+// 			}
+// 		default:
+// 			{
+// 				return nil, false, err
+// 			}
+// 		}
+// 	} else {
+// 		util.Debugf(ctx, "Aggregated metadata for `%s` found. Updating aggregated metadata...\n", *pckg.Name)
+// 		pckg.Assets = append(aggPckg.Assets, newAssets...)
+// 		found = true
+// 	}
 
-	successfulWrites, err := writeAggregatedMetadata(ctx, pckg)
-	return successfulWrites, found, err
-}
+// 	successfulWrites, err := writeAggregatedMetadata(ctx, pckg)
+// 	return successfulWrites, found, err
+// }
 
 // Reads an aggregated metadata entry in KV, ungzipping it and
 // unmarshalling it into a *packages.Package.
-func getAggregatedMetadata(key string) (*packages.Package, error) {
-	gzipBytes, err := read(key, aggregatedMetadataNamespaceID)
+// func getAggregatedMetadata(key string) (*packages.Package, error) {
+// 	gzipBytes, err := read(key, aggregatedMetadataNamespaceID)
 
-	if err != nil {
-		return nil, err
-	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	// unmarshal and ungzip
-	var p packages.Package
-	util.Check(json.Unmarshal(compress.UnGzip(gzipBytes), &p))
+// 	// unmarshal and ungzip
+// 	var p packages.Package
+// 	util.Check(json.Unmarshal(compress.UnGzip(gzipBytes), &p))
 
-	return &p, nil
-}
+// 	return &p, nil
+// }
 
 // Writes an aggregated metadata entry to KV, gzipping the bytes.
-func writeAggregatedMetadata(ctx context.Context, p *packages.Package) ([]string, error) {
-	// marshal package into JSON
-	v, err := p.Marshal()
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal KV package JSON: %s", *p.Name)
-	}
+// func writeAggregatedMetadata(ctx context.Context, p *packages.Package) ([]string, error) {
+// 	// marshal package into JSON
+// 	v, err := p.Marshal()
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to marshal KV package JSON: %s", *p.Name)
+// 	}
 
-	// gzip the bytes
-	req := &writeRequest{
-		name:  *p.Name,
-		key:   *p.Name,
-		value: compress.Gzip9Native(v),
-	}
+// 	// gzip the bytes
+// 	req := &writeRequest{
+// 		name:  *p.Name,
+// 		key:   *p.Name,
+// 		value: compress.Gzip9Native(v),
+// 	}
 
-	// write aggregated to KV
-	return encodeAndWriteKVBulk(ctx, []*writeRequest{req}, aggregatedMetadataNamespaceID, true)
-}
+// 	// write aggregated to KV
+// 	return encodeAndWriteKVBulk(ctx, []*writeRequest{req}, aggregatedMetadataNamespaceID, true)
+// }
