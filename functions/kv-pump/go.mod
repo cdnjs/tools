badbd2671fa0f960101b3c1ec28e43b0617e6f28
Sven Sauleau <sven@cloudflare.com>
2021-04-28 23:26:48 +0100

    add cloud functions and process-version

---
+++ functions/kv-pump/go.mod
@@ -0,0 +1,17 @@
+module github.com/cdnjs/tools/functions/kv-pump
+
+go 1.16
+
+require (
+	cloud.google.com/go v0.81.0 // indirect
+	cloud.google.com/go/storage v1.15.0 // indirect
+	github.com/cdnjs/tools v1.0.4-0.20210504151147-25f548edfc21 // indirect
+	github.com/cloudflare/cloudflare-go v0.16.0 // indirect
+	github.com/dlclark/regexp2 v1.2.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/pachyderm/ohmyglob v0.0.0-20190808212558-a8e61fd76805 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20210413134643-5e61552d6c78 // indirect
+	google.golang.org/api v0.45.0 // indirect
+	google.golang.org/grpc v1.37.0 // indirect
+)
--- kv/files.go
+++ kv/files.go
@@ -1,16 +1,16 @@
 package kv
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"path"
-	"time"
+// "context"
+// "fmt"
+// "net/http"
+// "os"
+// "path"
+// "time"
 
-	"github.com/cdnjs/tools/compress"
-	"github.com/cdnjs/tools/sri"
-	"github.com/cdnjs/tools/util"
+// "github.com/cdnjs/tools/compress"
+// "github.com/cdnjs/tools/sri"
+// "github.com/cdnjs/tools/util"
 )
 
 var (
@@ -38,127 +38,127 @@ func GetFiles(key string) ([]string, error) {
 	return listByPrefixNamesOnly(key+"/", filesNamespaceID)
 }
 
-// Gets the requests to update a number of files in KV, as well as the files' SRIs.
-// In order to do this, it will create a brotli and gzip version for each uncompressed file
-// that is not banned (ex. `.woff2`, `.br`, `.gz`).
-// Returns the list of requests for pushing SRIs and list of requests for pushing files to KV.
-func getFileWriteRequests(ctx context.Context, pkg, version, fullPathToVersion string, fromVersionPaths []string, srisOnly bool) ([]*writeRequest, []*writeRequest, error) {
-	baseVersionPath := path.Join(pkg, version)
-	var sriKVs, fileKVs []*writeRequest
+// // Gets the requests to update a number of files in KV, as well as the files' SRIs.
+// // In order to do this, it will create a brotli and gzip version for each uncompressed file
+// // that is not banned (ex. `.woff2`, `.br`, `.gz`).
+// // Returns the list of requests for pushing SRIs and list of requests for pushing files to KV.
+// func getFileWriteRequests(ctx context.Context, pkg, version, fullPathToVersion string, fromVersionPaths []string, srisOnly bool) ([]*writeRequest, []*writeRequest, error) {
+// 	baseVersionPath := path.Join(pkg, version)
+// 	var sriKVs, fileKVs []*writeRequest
 
-	for _, fromVersionPath := range fromVersionPaths {
-		ext := path.Ext(fromVersionPath)
-		if _, ok := ignored[ext]; ok {
-			util.Debugf(ctx, "file ignored from kv write: %s\n", fromVersionPath)
-			continue // ignore completely
-		}
-		fullPath := path.Join(fullPathToVersion, fromVersionPath)
-		baseFileKey := path.Join(baseVersionPath, fromVersionPath)
+// 	for _, fromVersionPath := range fromVersionPaths {
+// 		ext := path.Ext(fromVersionPath)
+// 		if _, ok := ignored[ext]; ok {
+// 			util.Debugf(ctx, "file ignored from kv write: %s\n", fromVersionPath)
+// 			continue // ignore completely
+// 		}
+// 		fullPath := path.Join(fullPathToVersion, fromVersionPath)
+// 		baseFileKey := path.Join(baseVersionPath, fromVersionPath)
 
-		// stat file
-		info, err := os.Stat(fullPath)
-		if err != nil {
-			return nil, nil, err
-		}
+// 		// stat file
+// 		info, err := os.Stat(fullPath)
+// 		if err != nil {
+// 			return nil, nil, err
+// 		}
 
-		// read file bytes
-		bytes, err := util.ReadLibFileSafely(fullPath)
-		if err != nil {
-			return nil, nil, err
-		}
+// 		// read file bytes
+// 		bytes, err := util.ReadLibFileSafely(fullPath)
+// 		if err != nil {
+// 			return nil, nil, err
+// 		}
 
-		if _, ok := calculateSRI[ext]; ok {
-			sriKVs = append(sriKVs, &writeRequest{
-				key:  baseFileKey,
-				name: fromVersionPath,
-				meta: &FileMetadata{
-					SRI: sri.CalculateSRI(bytes),
-				},
-			})
-		}
+// 		if _, ok := calculateSRI[ext]; ok {
+// 			sriKVs = append(sriKVs, &writeRequest{
+// 				key:  baseFileKey,
+// 				name: fromVersionPath,
+// 				meta: &FileMetadata{
+// 					SRI: sri.CalculateSRI(bytes),
+// 				},
+// 			})
+// 		}
 
-		if srisOnly {
-			continue
-		}
+// 		if srisOnly {
+// 			continue
+// 		}
 
-		// set metadata
-		lastModifiedTime := info.ModTime()
-		lastModifiedSeconds := lastModifiedTime.UnixNano() / int64(time.Second)
-		lastModifiedStr := lastModifiedTime.Format(http.TimeFormat)
-		etag := fmt.Sprintf("%x-%x", lastModifiedSeconds, info.Size())
+// 		// set metadata
+// 		lastModifiedTime := info.ModTime()
+// 		lastModifiedSeconds := lastModifiedTime.UnixNano() / int64(time.Second)
+// 		lastModifiedStr := lastModifiedTime.Format(http.TimeFormat)
+// 		etag := fmt.Sprintf("%x-%x", lastModifiedSeconds, info.Size())
 
-		fileMeta := &FileMetadata{
-			ETag:         etag,
-			LastModified: lastModifiedStr,
-		}
+// 		fileMeta := &FileMetadata{
+// 			ETag:         etag,
+// 			LastModified: lastModifiedStr,
+// 		}
 
-		if _, ok := doNotCompress[ext]; ok {
-			// will only insert uncompressed to KV
-			fileKVs = append(fileKVs, &writeRequest{
-				key:   baseFileKey,
-				name:  fromVersionPath,
-				value: bytes,
-				meta:  fileMeta,
-			})
-			continue
-		}
+// 		if _, ok := doNotCompress[ext]; ok {
+// 			// will only insert uncompressed to KV
+// 			fileKVs = append(fileKVs, &writeRequest{
+// 				key:   baseFileKey,
+// 				name:  fromVersionPath,
+// 				value: bytes,
+// 				meta:  fileMeta,
+// 			})
+// 			continue
+// 		}
 
-		// brotli
-		fileKVs = append(fileKVs, &writeRequest{
-			key:   baseFileKey + ".br",
-			name:  fromVersionPath + ".br",
-			value: compress.Brotli11CLI(ctx, fullPath),
-			meta:  fileMeta,
-		})
+// 		// brotli
+// 		fileKVs = append(fileKVs, &writeRequest{
+// 			key:   baseFileKey + ".br",
+// 			name:  fromVersionPath + ".br",
+// 			value: compress.Brotli11CLI(ctx, fullPath),
+// 			meta:  fileMeta,
+// 		})
 
-		// gzip
-		fileKVs = append(fileKVs, &writeRequest{
-			key:   baseFileKey + ".gz",
-			name:  fromVersionPath + ".gz",
-			value: compress.Gzip9Native(bytes),
-			meta:  fileMeta,
-		})
-	}
+// 		// gzip
+// 		fileKVs = append(fileKVs, &writeRequest{
+// 			key:   baseFileKey + ".gz",
+// 			name:  fromVersionPath + ".gz",
+// 			value: compress.Gzip9Native(bytes),
+// 			meta:  fileMeta,
+// 		})
+// 	}
 
-	return sriKVs, fileKVs, nil
-}
+// 	return sriKVs, fileKVs, nil
+// }
 
 // Updates KV with new version's files.
 // The []string of `fromVersionPaths` will already contain the optimized/minified files by now.
 // The function will return the list of SRIs pushed to KV and the list of all files pushed to KV.
-func updateKVFiles(ctx context.Context, pkg, version, fullPathToVersion string, fromVersionPaths []string, srisOnly, filesOnly, noPush, panicOversized bool) ([]string, []string, int, int, error) {
-	// create bulk of requests
-	sriReqs, fileReqs, err := getFileWriteRequests(ctx, pkg, version, fullPathToVersion, fromVersionPaths, srisOnly)
-	if err != nil {
-		return nil, nil, 0, 0, err
-	}
-	theoreticalSRIsKeys, theoreticalFilesKeys := len(sriReqs), len(fileReqs)
+// func updateKVFiles(ctx context.Context, pkg, version, fullPathToVersion string, fromVersionPaths []string, srisOnly, filesOnly, noPush, panicOversized bool) ([]string, []string, int, int, error) {
+// 	// create bulk of requests
+// 	sriReqs, fileReqs, err := getFileWriteRequests(ctx, pkg, version, fullPathToVersion, fromVersionPaths, srisOnly)
+// 	if err != nil {
+// 		return nil, nil, 0, 0, err
+// 	}
+// 	theoreticalSRIsKeys, theoreticalFilesKeys := len(sriReqs), len(fileReqs)
 
-	if noPush {
-		for _, f := range fileReqs {
-			if size := int64(len(f.value)); size > util.MaxFileSize {
-				if panicOversized {
-					panic(fmt.Sprintf("file request oversized: %s (%d)", f.key, size))
-				}
-				util.Infof(ctx, fmt.Sprintf("file request oversized: %s (%d)\n", f.key, size))
-			}
-		}
+// 	if noPush {
+// 		for _, f := range fileReqs {
+// 			if size := int64(len(f.value)); size > util.MaxFileSize {
+// 				if panicOversized {
+// 					panic(fmt.Sprintf("file request oversized: %s (%d)", f.key, size))
+// 				}
+// 				util.Infof(ctx, fmt.Sprintf("file request oversized: %s (%d)\n", f.key, size))
+// 			}
+// 		}
 
-		return nil, nil, theoreticalSRIsKeys, theoreticalFilesKeys, nil
-	}
+// 		return nil, nil, theoreticalSRIsKeys, theoreticalFilesKeys, nil
+// 	}
 
-	var successfulSRIWrites []string
-	if !filesOnly {
-		// write SRIs bulk to KV
-		successfulSRIWrites, err = encodeAndWriteKVBulk(ctx, sriReqs, srisNamespaceID, panicOversized)
-		if err != nil {
-			return nil, nil, 0, 0, err
-		}
-		if srisOnly {
-			return successfulSRIWrites, nil, theoreticalSRIsKeys, theoreticalFilesKeys, nil
-		}
-	}
+// 	var successfulSRIWrites []string
+// 	if !filesOnly {
+// 		// write SRIs bulk to KV
+// 		successfulSRIWrites, err = encodeAndWriteKVBulk(ctx, sriReqs, srisNamespaceID, panicOversized)
+// 		if err != nil {
+// 			return nil, nil, 0, 0, err
+// 		}
+// 		if srisOnly {
+// 			return successfulSRIWrites, nil, theoreticalSRIsKeys, theoreticalFilesKeys, nil
+// 		}
+// 	}
 
-	successfulFileWrites, err := encodeAndWriteKVBulk(ctx, fileReqs, filesNamespaceID, panicOversized)
-	return successfulSRIWrites, successfulFileWrites, theoreticalSRIsKeys, theoreticalFilesKeys, err
-}
+// 	successfulFileWrites, err := encodeAndWriteKVBulk(ctx, fileReqs, filesNamespaceID, panicOversized)
+// 	return successfulSRIWrites, successfulFileWrites, theoreticalSRIsKeys, theoreticalFilesKeys, err
+// }
