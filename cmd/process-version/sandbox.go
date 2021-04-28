090b0b41ea02d6868cebbaecdea286387c6062f7
Sven Sauleau <sven@cloudflare.com>
2021-04-28 23:26:48 +0100

    add cloud functions and process-version

---
+++ cmd/process-version/sandbox.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/pkg/errors"
+)
+
+func setupSandbox() (string, string, error) {
+	tmpDir := os.TempDir()
+	inDir, err := ioutil.TempDir(tmpDir, "in")
+	if err != nil {
+		return "", "", errors.Wrap(err, "failed to create in directory")
+	}
+	outDir, err := ioutil.TempDir(tmpDir, "out")
+	if err != nil {
+		return "", "", errors.Wrap(err, "failed to create out directory")
+	}
+
+	return inDir, outDir, nil
+}
+
+func runSandbox(in, out string) error {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"echo", "hello world"},
+		Tty:   false,
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	logs, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, logs)
+	return nil
+}
---
+++ functions/process-version/main.go
@@ -0,0 +1,111 @@
+package process_version
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"cloud.google.com/go/functions/metadata"
+	"cloud.google.com/go/pubsub"
+	"github.com/pkg/errors"
+)
+
+var (
+	TOPIC   = os.Getenv("PROCESSING_QUEUE")
+	PROJECT = os.Getenv("PROJECT")
+)
+
+// GCSEvent is the payload of a GCS event.
+type GCSEvent struct {
+	Kind                    string                 `json:"kind"`
+	ID                      string                 `json:"id"`
+	SelfLink                string                 `json:"selfLink"`
+	Name                    string                 `json:"name"`
+	Bucket                  string                 `json:"bucket"`
+	Generation              string                 `json:"generation"`
+	Metageneration          string                 `json:"metageneration"`
+	ContentType             string                 `json:"contentType"`
+	TimeCreated             time.Time              `json:"timeCreated"`
+	Updated                 time.Time              `json:"updated"`
+	TemporaryHold           bool                   `json:"temporaryHold"`
+	EventBasedHold          bool                   `json:"eventBasedHold"`
+	RetentionExpirationTime time.Time              `json:"retentionExpirationTime"`
+	StorageClass            string                 `json:"storageClass"`
+	TimeStorageClassUpdated time.Time              `json:"timeStorageClassUpdated"`
+	Size                    string                 `json:"size"`
+	MD5Hash                 string                 `json:"md5Hash"`
+	MediaLink               string                 `json:"mediaLink"`
+	ContentEncoding         string                 `json:"contentEncoding"`
+	ContentDisposition      string                 `json:"contentDisposition"`
+	CacheControl            string                 `json:"cacheControl"`
+	Metadata                map[string]interface{} `json:"metadata"`
+	CRC32C                  string                 `json:"crc32c"`
+	ComponentCount          int                    `json:"componentCount"`
+	Etag                    string                 `json:"etag"`
+	CustomerEncryption      struct {
+		EncryptionAlgorithm string `json:"encryptionAlgorithm"`
+		KeySha256           string `json:"keySha256"`
+	}
+	KMSKeyName    string `json:"kmsKeyName"`
+	ResourceState string `json:"resourceState"`
+}
+
+func Invoke(ctx context.Context, e GCSEvent) error {
+	meta, err := metadata.FromContext(ctx)
+	if err != nil {
+		return fmt.Errorf("metadata.FromContext: %v", err)
+	}
+	log.Printf("Event ID: %v\n", meta.EventID)
+	log.Printf("Event type: %v\n", meta.EventType)
+	log.Printf("File: %v\n", e.Name)
+	log.Printf("Metadata: %v\n", e.Metadata)
+
+	pkg := e.Metadata["package"].(string)
+	version := e.Metadata["version"].(string)
+
+	if err := publish(e.SelfLink, pkg, version); err != nil {
+		return fmt.Errorf("failed to publish: %v", err)
+	}
+	return nil
+}
+
+// FIXME: share with process-version
+type Message struct {
+	OutgoingSignedURL string `json:"outgoingSignedURL"`
+	Tar               string `json:"tar"`
+	Pkg               string `json:"package"`
+	Version           string `json:"version"`
+}
+
+func publish(tar, pkg, version string) error {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, PROJECT)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+	t := client.Topic(TOPIC)
+
+	msg := Message{
+		OutgoingSignedURL: "TODO",
+		Tar:               tar,
+		Pkg:               pkg,
+		Version:           version,
+	}
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("could not marshal message: %s", err)
+	}
+	result := t.Publish(ctx, &pubsub.Message{Data: bytes})
+
+	// The Get method blocks until a server-generated ID or
+	// an error is returned for the published message.
+	id, err := result.Get(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to publish")
+	}
+	log.Printf("Published msg ID: %v\n", id)
+	return nil
+}
