346471a900ce110ef04c7af4aa902e723f4de570
Sven Sauleau <sven@cloudflare.com>
2021-04-28 23:26:48 +0100

    add cloud functions and process-version

--- kv/aggregate.go
+++ kv/aggregate.go
@@ -4,25 +4,34 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/cdnjs/tools/compress"
+	"github.com/cdnjs/tools/packages"
 	"github.com/cdnjs/tools/util"
 
-	"github.com/cdnjs/tools/packages"
+	cloudflare "github.com/cloudflare/cloudflare-go"
 )
 
 // UpdateAggregatedMetadata updates a package's KV entry for aggregated metadata.
 // Returns the keys written to KV, whether the existing entry was found, and if there were any errors.
-func UpdateAggregatedMetadata(ctx context.Context, pckg *packages.Package, newAssets []packages.Asset) ([]string, bool, error) {
-	aggPckg, err := getAggregatedMetadata(*pckg.Name)
+func UpdateAggregatedMetadata(api *cloudflare.API, ctx context.Context,
+	pkg *packages.Package, newVersion string, newAssets packages.Asset) ([]string, bool, error) {
+	aggPkg, err := getAggregatedMetadata(api, *pkg.Name)
+
+	if aggPkg == nil {
+		// pkg has never been aggregated
+		aggPkg = pkg
+	}
+
 	var found bool
 	if err != nil {
 		switch err.(type) {
 		case KeyNotFoundError:
 			{
 				// key not found (new package)
-				util.Debugf(ctx, "KV key `%s` not found, inserting aggregated metadata...\n", *pckg.Name)
-				pckg.Assets = newAssets
+				log.Printf("KV key `%s` not found, inserting aggregated metadata...\n", *pkg.Name)
+				aggPkg.Assets = []packages.Asset{newAssets}
 			}
 		default:
 			{
@@ -30,19 +39,23 @@ func UpdateAggregatedMetadata(ctx context.Context, pckg *packages.Package, newAs
 			}
 		}
 	} else {
-		util.Debugf(ctx, "Aggregated metadata for `%s` found. Updating aggregated metadata...\n", *pckg.Name)
-		pckg.Assets = append(aggPckg.Assets, newAssets...)
+		log.Printf("Aggregated metadata for `%s` found. Updating aggregated metadata...\n", *pkg.Name)
+
+		if !aggPkg.HasVersion(newVersion) {
+			aggPkg.Assets = append(aggPkg.Assets, newAssets)
+		}
 		found = true
 	}
+	aggPkg.Version = &newVersion
 
-	successfulWrites, err := writeAggregatedMetadata(ctx, pckg)
+	successfulWrites, err := writeAggregatedMetadata(ctx, api, aggPkg)
 	return successfulWrites, found, err
 }
 
 // Reads an aggregated metadata entry in KV, ungzipping it and
 // unmarshalling it into a *packages.Package.
-func getAggregatedMetadata(key string) (*packages.Package, error) {
-	gzipBytes, err := read(key, aggregatedMetadataNamespaceID)
+func getAggregatedMetadata(api *cloudflare.API, key string) (*packages.Package, error) {
+	gzipBytes, err := read(api, key, aggregatedMetadataNamespaceID)
 
 	if err != nil {
 		return nil, err
@@ -56,7 +69,7 @@ func getAggregatedMetadata(key string) (*packages.Package, error) {
 }
 
 // Writes an aggregated metadata entry to KV, gzipping the bytes.
-func writeAggregatedMetadata(ctx context.Context, p *packages.Package) ([]string, error) {
+func writeAggregatedMetadata(ctx context.Context, api *cloudflare.API, p *packages.Package) ([]string, error) {
 	// marshal package into JSON
 	v, err := p.Marshal()
 	if err != nil {
@@ -64,12 +77,12 @@ func writeAggregatedMetadata(ctx context.Context, p *packages.Package) ([]string
 	}
 
 	// gzip the bytes
-	req := &writeRequest{
-		name:  *p.Name,
-		key:   *p.Name,
-		value: compress.Gzip9Native(v),
+	req := &WriteRequest{
+		Name:  *p.Name,
+		Key:   *p.Name,
+		Value: compress.Gzip9Bytes(v),
 	}
 
 	// write aggregated to KV
-	return encodeAndWriteKVBulk(ctx, []*writeRequest{req}, aggregatedMetadataNamespaceID, true)
+	return EncodeAndWriteKVBulk(ctx, api, []*WriteRequest{req}, aggregatedMetadataNamespaceID, true)
 }
