1548c71a559163a601c24cda58b5c06fa9f7c44e
Sven Sauleau <sven@cloudflare.com>
2021-04-28 23:26:48 +0100

    add cloud functions and process-version

---
+++ functions/check-pkg-updates/npm.go
@@ -0,0 +1,112 @@
+package check_pkg_updates
+
+import (
+	"context"
+	"path"
+	"sort"
+
+	"github.com/cdnjs/tools/npm"
+	"github.com/cdnjs/tools/packages"
+	"github.com/cdnjs/tools/util"
+)
+
+func updateNpm(ctx context.Context, pckg *packages.Package) ([]newVersionToCommit, []version) {
+	var newVersionsToCommit []newVersionToCommit
+	var allVersions []version
+
+	existingVersionSet, err := pckg.Versions()
+	// TODO: handle error
+	if err != nil {
+		panic(err)
+	}
+	util.Debugf(ctx, "existing npm versions: %v\n", existingVersionSet)
+
+	npmVersions, _ := npm.GetVersions(ctx, *pckg.Autoupdate.Target)
+	lastExistingVersion, allExisting := npm.GetMostRecentExistingVersion(ctx, existingVersionSet, npmVersions)
+
+	// add all existing versions to all versions list
+	for _, v := range allExisting {
+		allVersions = append(allVersions, version(v))
+	}
+
+	if lastExistingVersion != nil {
+		util.Debugf(ctx, "last existing version: %s\n", lastExistingVersion.Version)
+
+		versionDiff := npmVersionDiff(npmVersions, existingVersionSet)
+		sort.Sort(npm.ByTimeStamp(versionDiff))
+
+		newNpmVersions := make([]npm.Version, 0)
+
+		for i := len(versionDiff) - 1; i >= 0; i-- {
+			v := versionDiff[i]
+			if v.TimeStamp.After(lastExistingVersion.TimeStamp) {
+				newNpmVersions = append(newNpmVersions, v)
+			}
+		}
+
+		sort.Sort(sort.Reverse(npm.ByTimeStamp(npmVersions)))
+
+		newVersionsToCommit = doUpdateNpm(ctx, pckg, newNpmVersions)
+	} else {
+		if len(existingVersionSet) > 0 {
+			// all existing versions are not on npm anymore
+			util.Debugf(ctx, "all existing versions not on npm: %s\n", *pckg.Name)
+		}
+		// Import all the versions since we have no current npm versions locally.
+		// Limit the number of version to an arbitrary number to avoid publishing
+		// too many outdated versions.
+		sort.Sort(sort.Reverse(npm.ByTimeStamp(npmVersions)))
+
+		if len(npmVersions) > util.ImportAllMaxVersions {
+			npmVersions = npmVersions[len(npmVersions)-util.ImportAllMaxVersions:]
+		}
+
+		// Reverse the array to have the older versions first
+		// It matters when we will commit the updates
+		sort.Sort(sort.Reverse(npm.ByTimeStamp(npmVersions)))
+
+		newVersionsToCommit = doUpdateNpm(ctx, pckg, npmVersions)
+	}
+
+	// add all new versions to list of all versions
+	for _, v := range newVersionsToCommit {
+		allVersions = append(allVersions, version(v))
+	}
+
+	return newVersionsToCommit, allVersions
+}
+
+func doUpdateNpm(ctx context.Context, pckg *packages.Package, versions []npm.Version) []newVersionToCommit {
+	newVersionsToCommit := make([]newVersionToCommit, 0)
+
+	if len(versions) == 0 {
+		return newVersionsToCommit
+	}
+
+	for _, version := range versions {
+		util.Infof(ctx, "new version detected: %s", version.Version)
+		tarball := npm.DownloadTar(ctx, version.Tarball)
+		if err := storeGCS(path.Base(version.Tarball), tarball, pckg, version); err != nil {
+			panic(err)
+		}
+	}
+
+	return newVersionsToCommit
+}
+
+func npmVersionDiff(a []npm.Version, b []string) []npm.Version {
+	diff := make([]npm.Version, 0)
+	m := make(map[string]bool)
+
+	for _, item := range b {
+		m[item] = true
+	}
+
+	for _, item := range a {
+		if _, ok := m[item.Version]; !ok {
+			diff = append(diff, item)
+		}
+	}
+
+	return diff
+}
---
+++ functions/kv-pump/kv.go
@@ -0,0 +1,134 @@
+package kv_pump
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/cdnjs/tools/sentry"
+	"github.com/cdnjs/tools/util"
+
+	cloudflare "github.com/cloudflare/cloudflare-go"
+)
+
+const (
+	// workaround for now since cloudflare's API does not currently
+	// return a cloudflare.Response object for api.ReadWorkersKV
+	keyNotFound    = "key not found"
+	authError      = "Authentication error"
+	serviceFailure = "service failure"
+)
+
+type FileMetadata struct {
+	ETag         string `json:"etag,omitempty"`
+	LastModified string `json:"last_modified,omitempty"`
+	SRI          string `json:"sri,omitempty"`
+}
+
+// Represents a KV write request, consisting of
+// a string key, a []byte value, and file metadata.
+// The name field is used to identify this write request
+// with a human-readable friendly name.
+type writeRequest struct {
+	key   string
+	name  string
+	value []byte
+	meta  *FileMetadata
+}
+
+// Encodes key-value pairs to base64 and writes them to KV in multiple bulk requests.
+// Returns the list of human-readable names of successful writes.
+func encodeAndWriteKVBulk(ctx context.Context, cfapi *cloudflare.API, kvs []*writeRequest, namespaceID string, panicOversized bool) ([]string, error) {
+	var bulkWrites []cloudflare.WorkersKVBulkWriteRequest
+	var bulkWrite []*cloudflare.WorkersKVPair
+	var successfulWrites []string
+	var totalSize, totalKeys int64
+
+	for _, kv := range kvs {
+		if unencodedSize := int64(len(kv.value)); unencodedSize > util.MaxFileSize {
+			log.Printf("ignoring oversized file: %s (%d)\n", kv.key, unencodedSize)
+			sentry.NotifyError(fmt.Errorf("ignoring oversized file: %s (%d)", kv.key, unencodedSize))
+			if panicOversized {
+				panic(fmt.Sprintf("oversized file: %s (%d)", kv.key, unencodedSize))
+			}
+			continue
+		}
+		// Note that after encoding in base64 the size may get larger, but after decoding
+		// it will be reduced, so it is okay if the size is larger than util.MaxFileSize after encoding base64.
+		// However, we still need to check for the KV bulk request limit of 100MiB.
+		encodedValue := encodeToBase64(kv.value)
+		size := int64(len(encodedValue))
+		writePair := &cloudflare.WorkersKVPair{
+			Key:    kv.key,
+			Value:  encodedValue,
+			Base64: true,
+		}
+		if kv.meta != nil {
+			// Marshal metadata into JSON bytes.
+			bytes, err := json.Marshal(kv.meta)
+			if err != nil {
+				return nil, err
+			}
+			metasize := int64(len(bytes))
+			if metasize > util.MaxMetadataSize {
+				log.Printf("ignoring oversized metadata: %s (%d)\n", kv.key, metasize)
+				sentry.NotifyError(fmt.Errorf("oversized metadata: %s (%d) - %s", kv.key, metasize, bytes))
+				if panicOversized {
+					panic(fmt.Sprintf("oversized metadata: %s (%d)", kv.key, metasize))
+				}
+				continue
+			}
+			log.Printf("writing metadata: %s\n", bytes)
+			writePair.Metadata = kv.meta
+			size += metasize
+		}
+		if totalSize+size > util.MaxBulkWritePayload || totalKeys == util.MaxBulkKeys {
+			// Create a new bulk since we are over a limit.
+			bulkWrites = append(bulkWrites, bulkWrite)
+			bulkWrite = []*cloudflare.WorkersKVPair{}
+			totalSize = 0
+			totalKeys = 0
+		}
+		bulkWrite = append(bulkWrite, writePair)
+		successfulWrites = append(successfulWrites, kv.name)
+		totalSize += size
+		totalKeys++
+	}
+	bulkWrites = append(bulkWrites, bulkWrite)
+
+	for i, b := range bulkWrites {
+		log.Printf("writing bulk %d/%d (keys=%d)...\n", i+1, len(bulkWrites), len(b))
+		for j := 0; j < util.MaxKVAttempts; j++ {
+			r, err := cfapi.WriteWorkersKVBulk(ctx, namespaceID, b)
+
+			// check for service failure and retry
+			if err != nil && strings.Contains(err.Error(), serviceFailure) {
+				if j == util.MaxKVAttempts-1 {
+					return nil, err // no more attempts
+				}
+				continue // retry
+			}
+
+			if err = checkSuccess(r, err); err != nil {
+				return nil, err
+			}
+
+			break
+		}
+	}
+
+	return successfulWrites, nil
+}
+
+// Ensure a response is successful and the error is nil.
+func checkSuccess(r cloudflare.Response, err error) error {
+	if err != nil {
+		return err
+	}
+	if !r.Success {
+		return fmt.Errorf("kv fail: %v", r)
+	}
+	return nil
+}
